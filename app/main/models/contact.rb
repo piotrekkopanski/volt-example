125a85bf0b4a8d5624e64354b9c2fd906f41ee76
piotrkopanski <piotrkopanski@interia.pl>
2017-09-16 10:44:58 +0200

    add validation

--- app/main/models/contact.rb
+++ app/main/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < Volt::Model
-
+  validate :name, presence: true
+  validate :last_name, presence: true
+  validate :email, presence: true
+  validate :phone, presence: true, length: 9, numericality: true
 end
